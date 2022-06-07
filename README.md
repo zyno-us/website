3761a34f2402ce82de6155fbfa5766b452406b56
Nebula <nebulalol@icloud.com>
2022-06-07 16:15:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+<img width="200" height="200" src="https://raw.githubusercontent.com/zyno-us/.github/main/zyno.png">
+</p>
+<h1 align="center">Zyno Website</h1>
+<p align="center">🌐 Zyno's Website made using Svelte and Vite.</p>
+
+## Run
+1. Install Node.js and `pnpm`.
+2. Clone this repository.
+3. Run `pnpm dev` to start the development server.
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Button from './lib/Button.svelte'
+</script>
+
+<main>
+  <div class="center">
+    <h1>Welome to Zyno</h1>
+    <p>Take back your privacy.</p>
+    <br>
+    <Button url="https://place.zyno.us" label="Zyno Place" />
+    <Button url="https://github.com/zyno-us/dns" label="ZynoDNS" />
+  </div>
+</main>
+
+<style>
+  :root {
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .center { 
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+</style>
